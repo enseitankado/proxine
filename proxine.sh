31ba34ce9d61955c8167a7e8441b8d22e02e0653
enseitankado <ozgurkoca@gmail.com>
2022-09-10 17:13:18 -0400

    update

--- proxine.sh
+++ proxine.sh
@@ -3,9 +3,9 @@ if [ $# -eq 0 ]; then
    echo ""
    echo "No arguments provided."
    echo ""
-   echo "  Usage: ./awesome-proxies.sh http/https/socks4/socks5";
+   echo "  Usage: ./proxine.sh http/https/socks4/socks5";
    echo ""
-   echo "Example: ./awesome-proxies.sh http"
+   echo "Example: ./proxine.sh http"
    exit 1
 fi
 ./utils/ap-main.sh "$@" | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}\:[0-9]{1,5}" | sort -u
