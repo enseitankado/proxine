ab364a86cf8871f63131bc5691e6b6eaa5300e4e
enseitankado <ozgurkoca@gmail.com>
2022-09-18 23:13:21 +0100

    update

--- proxy.sh
+++ proxy.sh
@@ -10,6 +10,7 @@ fi
 
 if ! [[ "$1" =~ ^(http|https|socks4|socks5)$ ]]; then
 	printf "\n Unsupported proxy type! \n\n Please use one of these: http/https/socks4/socks5\n"
+	exit 2
 fi
 
 ./utils/core.sh $@ | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}\:[0-9]{1,5}" | sort -u
