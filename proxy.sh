ab72eeca936d18fa419379d121ae8f68f6144188
enseitankado <ozgurkoca@gmail.com>
2022-09-17 21:42:37 -0400

    update

--- proxy.sh
+++ proxy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
    echo ""
-   echo "No arguments provided."
-   echo ""
    echo "  Usage: ./proxine.sh http/https/socks4/socks5";
    echo ""
-   echo "Example: ./proxine.sh http"
+   echo "  Example: ./proxine.sh http"
+   echo ""
    exit 1
 fi
 ./utils/ap-main.sh "$@" | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}\:[0-9]{1,5}" | sort -u
