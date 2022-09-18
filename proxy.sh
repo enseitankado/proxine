7ee32db2108fd4e8cb902b6e4846f063f752c406
enseitankado <ozgurkoca@gmail.com>
2022-09-18 23:09:09 +0100

    update

--- proxy.sh
+++ proxy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
    echo ""
-   echo "  Usage: ./proxine.sh http/https/socks4/socks5";
+   echo "  Usage: ./proxine.sh http/https/socks4/socks5"
    echo ""
    echo "  Example: ./proxine.sh http"
    echo ""
    exit 1
 fi
-./utils/ap-main.sh "$@" | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}\:[0-9]{1,5}" | sort -u
+
+if ! [[ "$1" =~ ^(http|https|socks4|socks5)$ ]]; then
+	printf "\n Unsupported proxy type! \n\n Please use one of these: http/https/socks4/socks5\n"
+fi
+
+./utils/core.sh $@ | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}\:[0-9]{1,5}" | sort -u
